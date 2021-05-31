4a1dceec398b9440351852177476c4dc1e3bfecf
Chris Bennett <47375905+chrisbennett-Bene@users.noreply.github.com>
2021-05-31 19:08:27 +1000

    updates

---
+++ AdminThemeTweaker.js
@@ -0,0 +1,50 @@
+﻿var get           = function (selector, scope) {
+                    scope = scope ? scope : document; 
+                    return scope.querySelector(selector);
+                    };
+var getById       = function (selector, scope) {
+                    scope = scope ? scope : document; 
+                    return scope.getElementById(selector);
+                    };
+var getAll        = function (selector, scope) {
+                    scope = scope ? scope : document; 
+                    return scope.querySelectorAll(selector);
+                    };
+
+window.addEventListener('load', 
+    function() {
+
+    var color = getAll('.colorpicker');
+        color.forEach(function (color, index) {
+            if (color.type == "text" && color.value) {
+                color.type = "color";
+				color.setAttribute('title', '#rrggbb or #rgb - Right-click to copy, paste or remove hex code');
+            } 
+        });
+	
+    window.addEventListener('contextmenu', function (event) {
+        if (event.target.classList.contains('colorpicker')){
+            if (event.target.type == "color" && event.target.value) {event.target.type = "text";
+						} 
+        }
+		}, false);	
+		
+    var initColorpicker =  function (event) {
+        if (event.target.classList.contains('colorpicker')){
+
+            if (event.target.type == "text" && event.target.value && event.target.value.length == 7) {
+                event.target.type = "color";
+
+            } else if (event.target.type == "text" && event.target.value.length == 4){
+            var hex = event.target.value;
+						hex = "#" + hex.charAt(1) + hex.charAt(1) + hex.charAt(2) + hex.charAt(2) + hex.charAt(3) + hex.charAt(3);
+						event.target.value = hex;
+						}
+         }
+    };
+
+    // Add event listeners for colorpicker
+    window.addEventListener('change', initColorpicker, false);
+    window.addEventListener('mouseout', initColorpicker, false);
+  
+	}, false);
